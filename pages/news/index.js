13782c11c6d1dee03260521152ce7c746b6e97b5
marcosfil <marcosfilho222@gmail.com>
2023-01-03 19:05:09 +0100

    update

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,19 @@
 // our - domain.com /news
 
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <a href="/news/nextjs-is-great">NextJs is A Great Framework</a>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
